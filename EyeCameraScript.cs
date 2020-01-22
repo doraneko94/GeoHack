269dad750546da6c8ada7e083569c07d72ad4fca
doraneko94 <shuntaro94@gmail.com>
2020-01-22 09:09:49 +0900

    add CS files

---
+++ EyeCameraScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EyeCameraScript : MonoBehaviour
+{
+    public GameObject target = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 position = target.transform.position;
+        Vector3 forward = target.transform.forward;
+        position.y += 2f;
+        position += forward * -7f;
+        transform.position = position;
+
+        float arg_new = ArcTan(forward);
+        float arg_old = ArcTan(transform.forward);
+        transform.Rotate(0f, (arg_new - arg_old) % 360f, 0f);
+    }
+
+    float ArcTan(Vector3 forward)
+    {
+        float f_x = forward.x;
+        float f_z = forward.z;
+        float arg = Mathf.Atan(f_x / f_z) / (2f * Mathf.PI) * 360f;
+
+        if (f_z < 0) arg += 180f;
+
+        return arg;
+    }
+}
---
+++ PlayerController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 3.0f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("up")) {
+            transform.position += transform.forward * speed * Time.deltaTime;
+        }
+        if (Input.GetKey("down")) {
+            transform.position -= transform.forward * speed * Time.deltaTime;
+        }
+        if (Input.GetKey("right")) {
+            transform.Rotate(0f, speed * Time.deltaTime * 10f, 0f);
+        }
+        if (Input.GetKey("left")) {
+            transform.Rotate(0f, -speed * Time.deltaTime * 10f, 0f);
+        }
+        if (Input.GetKey(KeyCode.X)) {
+            transform.Rotate(speed * Time.deltaTime * 50f, 0f, 0f);
+        }
+        if (Input.GetKey(KeyCode.Z)) {
+            transform.Rotate(0f, 0f, speed * Time.deltaTime * 50f);
+        }
+    }
+
+    Vector3 ForwardXZ(Vector3 forward) {
+        float f_x = forward.x;
+        float f_z = forward.z;
+        float norm = Mathf.Sqrt(f_x * f_x + f_z * f_z);
+        Vector3 vec = new Vector3(f_x / norm, 0f, f_z / norm);
+        return vec;
+    }
+}
